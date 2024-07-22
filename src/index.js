4667f42512faf0c0a4dab853bab9e012fda326b5
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-07-22 12:57:07 +0530

    utils implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 //require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongodb connection error:", error);
+  });
 
 /*import express from "express";
 const app = express();
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+  constructor(message = "Success", data, statusCode) {
+    this.data = data;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
