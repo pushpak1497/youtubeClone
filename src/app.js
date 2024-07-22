0b8502903797a14bd2391ca0183dd1e874412cc0
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-07-22 12:57:23 +0530

    utils implementation

--- src/app.js
+++ src/app.js
@@ -14,4 +14,6 @@ app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ extended: true, limit: "30kb" }));
 app.use(express.static("public"));
 
+app.use(cookieParser({}));
+
 export { app };
