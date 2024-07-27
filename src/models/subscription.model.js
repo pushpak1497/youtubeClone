c7a64e798298afb3d8f86ea7bba996848ea4635f
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-07-27 11:50:08 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: { type: Schema.Types.ObjectId, ref: "User" }, //one who is subscribing
+    channel: { type: Schema.Types.ObjectId, ref: "User" }, //one who is the owner
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
